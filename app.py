c2e1552c9e5803d4c1f304563c933f504d183a6a
shisar86 <shivaramshisar86@gmail.com>
2024-12-19 12:43:05 +0530

    requirements installed

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from dotenv import load_dotenv
 load_dotenv() ##load all the environment variables from .env
 import streamlit as st
 import os
+import PIL as Image
+import google.generativeai as genai
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
  
